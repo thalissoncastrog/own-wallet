5c9351a79ff3eff45434044fb44ccfb860a493fd
Thalisson Castro <adaothalisson@hotmail.com>
2024-06-27 23:47:58 -0300

    deleted some unused files

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -8,8 +8,3 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     GRANT USAGE, CREATE ON SCHEMA PROD TO "$USER_NAME";
     GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA PROD TO "$USER_NAME";
 EOSQL
-
-psql -v ON_ERROR_STOP=1 --username "$USER_NAME" --dbname "$POSTGRES_DB" -w '$USER_PASSWORD' <<-EOSQL
-    \i /docker-entrypoint-initdb.d/sql/create_tables.sql
-    \i /docker-entrypoint-initdb.d/sql/insert_initial_data.sql
-EOSQL
